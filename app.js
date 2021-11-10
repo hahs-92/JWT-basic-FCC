346b5bb91e7af129f0c5fc795c8f2c554b237952
Hugo Alexander Hernandez Serrano <alexandre_2420@outlook.com>
2021-11-10 08:56:58 -0500

    setup project

---
+++ app.js
@@ -0,0 +1,34 @@
+//async errors
+require('express-async-errors')
+//config
+const { config } = require('./config/config')
+//middlewares
+const notFoundMiddleware = require('./middlewares/not-found')
+const errorHandlerMiddleware = require('./middlewares/error-handler')
+
+const port = config.port || 5000
+
+const express = require('express')
+const app = express();
+
+// middleware
+app.use(express.static('./public'))
+app.use(express.json())
+
+//routes
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+start()
